413288557f5b67cf08225891d2510c3f1746ba30
akshaychavan123 <chavanakshaykumar10@gmail.com>
2022-12-09 07:26:11 -0800

    images attaching to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_one_attached :image
     # validates :name, :email , presence: true 
     # validates :password , uniqueness: true 
     has_many :smaccounts  
