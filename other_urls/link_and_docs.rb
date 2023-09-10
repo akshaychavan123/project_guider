26ae0028e77f5f62e207a9e4add1eba89063f95d
akshay <akshay.kumar@deeporion.com>
2023-09-11 01:24:19 +0530

    change user passwords in postgres database

---
+++ other_urls/link_and_docs.rb
@@ -0,0 +1,5 @@
+for changing the users password in postgres database if you have postgres in your system
+
+    https://wajeeh-ahsan.medium.com/fatal-peer-authentication-failed-for-user-postgres-954e061c7368
+
+    
