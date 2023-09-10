06bce7d60c27c1cca09792791be2e6d2a0ae1f62
akshay <akshay.kumar@deeporion.com>
2023-09-11 01:38:29 +0530

    amazon s3 storage

--- other_urls/link_and_docs.rb
+++ other_urls/link_and_docs.rb
@@ -3,3 +3,7 @@ for changing the users password in postgres database if you have postgres in you
     https://wajeeh-ahsan.medium.com/fatal-peer-authentication-failed-for-user-postgres-954e061c7368
 
     
+
+    storing images in s3
+    https://youtu.be/OWBWXOcx1rU?si=LxdVVD6i2uGwufFu
+    after this follow image
