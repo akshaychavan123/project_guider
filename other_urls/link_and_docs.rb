62c27db9508dbab93e9c6c8691c786a0600c03f9
akshaychavan123 <112326327+akshaychavan123@users.noreply.github.com>
2024-01-29 11:05:15 +0530

    link and ai tools for rails

--- other_urls/link_and_docs.rb
+++ other_urls/link_and_docs.rb
@@ -7,3 +7,22 @@ for changing the users password in postgres database if you have postgres in you
     storing images in s3
     https://youtu.be/OWBWXOcx1rU?si=LxdVVD6i2uGwufFu
     after this follow image
+
+
+
+vscode extension for code generate 
+
+Blackbox.ai 
+
+and below is my chatprofile on akshay.kumar@deeporion.com
+https://www.blackbox.ai/agent/AkshayKumarVNnuIpv
+
+
+
+auto gem and other imp library configuration and imporatnt site railsbytes
+
+https://railsbytes.com/public/templates?page=17
+
+another site for rails  jumpstartrails
+
+https://jumpstartrails.com/
