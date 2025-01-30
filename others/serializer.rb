d52026fce52180bf180bba29cd528f7894a49d50
akshaychavan123 <112326327+akshaychavan123@users.noreply.github.com>
2025-01-31 01:04:16 +0530

    Update serializer.rb

--- others/serializer.rb
+++ others/serializer.rb
@@ -83,4 +83,46 @@ class SalaryInfoSerializer < ActiveModel::Serializer
         object.addition.as_json(except: [:created_at, :updated_at])
     end
 end
+
+
+        ===============================================================================================================================================
+When in developement.rb file config.active_storage :local and in production is anything than local then for active storage attachment
+class BlogSerializer < ActiveModel::Serializer
+  attributes :id, :title, :category, :description, :path_name, :card_home_url, :card_insights_url, :banner_url, :body,
+   :visibility, :publish_date, :card_home_url_alt, :card_insights_url_alt, :banner_url_alt
+
+  attribute :publisher do
+    if (user = User.find_by(id: object.publisher_id))
+      {
+        id: user.id,
+        name: user.name,
+        profile_picture: user.image.attached? ? s3_url(user.image) : nil
+      }
+    else
+      nil
+    end
+  end
+
+  private
+
+  def serialize_image(image)
+    {
+      name: image.filename.to_s,
+      content_type: image.content_type,
+      url: s3_url(image)
+    }
+  end
+
+  def s3_url(attachment)
+    return nil if attachment.nil?
+  
+    if Rails.env.production? || Rails.application.config.active_storage.service == :amazon
+      url_parts = attachment.url.split('?', 2)
+      base_url = url_parts.first
+      base_url
+    else
+      Rails.application.routes.url_helpers.rails_blob_url(attachment, host: "localhost:3000")
+    end
+  end  
+end  
         
