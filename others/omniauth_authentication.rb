99a8aee9abc55882b9c4bf272c3466019a306220
akshaychavan123 <112326327+akshaychavan123@users.noreply.github.com>
2025-03-20 15:36:21 +0530

    google login frontend id_token

--- others/omniauth_authentication.rb
+++ others/omniauth_authentication.rb
@@ -123,4 +123,80 @@ devise_for :users , controllers: {
 config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :github, 'd4f83ba9183897b45ede', '5e1488c6a059f45140b0552fecdc8abcf88a62c2'
   config.omniauth :google_oauth2, '968223638131-n06nldjq1mdhlrmasvatqmg1hpt118qm.apps.googleusercontent.com', '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'
+=================================================================================================================================================================================
+
+the simplest solution for google authentication or login was you dont need any creds or initailizer file differentely 
+just add below post spi and get id_token from frontend,,... exract user details from id_token and save it in model or from anywhere
+
+require 'httparty'                                                             
+require 'json'
+class Api::V1::SessionsController < ApplicationController
+  include HTTParty
+
+  def google_auth
+    url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{params["id_token"]}"                  
+    response = HTTParty.get(url)                   
+    @user = User.create_user_for_google(response.parsed_response)      
+
+    if @user.new_record?
+      if @user.save
+        payload = { user_id: @user.id }
+        token = JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
+        render json: { jwt: token, user: @user }, status: :ok
+      else
+        render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      payload = { user_id: @user.id }
+      token = JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
+      render json: { jwt: token, user: @user }, status: :ok
+    end
+  end
+end
+
+
+in user.rb
+
+  def self.create_user_for_google(data)
+    where(uid: data["email"]).first_or_initialize.tap do |user|
+      user.provider="google_oauth2"
+      user.uid=data["email"]
+      user.email=data["email"]
+      user.name = data["name"]
+      user.password=generate_secure_password
+      user.password_confirmation=user.password
+      user.save!
+    end
+  end  
+
+
+in gemfile
+
+gem 'omniauth'
+gem 'omniauth-google-oauth2'
+gem 'omniauth-rails_csrf_protection'
+
+and thats all.....
+
+
+to check above code go to 
+
+https://developers.google.com/oauthplayground/
+
+    from here select below two scopes 
+https://www.googleapis.com/auth/userinfo.email
+availablehttps://www.googleapis.com/auth/userinfo.profile
+
+
+    click authorize apis
+
+select login email from here choose account from here and after that it will give you authorization code 
+
+click on exchange authorization code for token 
+
+it will give you response copy id_token from here
+
+
+
+    
 
